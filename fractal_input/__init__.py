63b195c7c117a2c5a74cd63134acd3d799a8bdf6
Jeferson Daniel <jeferson.daniel412@gmail.com>
2024-02-19 13:57:26 -0300

    fix: fix __init__.py

--- fractal_input/__init__.py
+++ fractal_input/__init__.py
@@ -1,3 +1,3 @@
-from version import __version__ # noqa
+from .version import __version__ # noqa
 from .input_handler import InputHandler # noqa
 from .node import Node, ObjectNode, ListNode, DatetimeNode # noqa
